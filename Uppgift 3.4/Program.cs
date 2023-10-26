dce0180091b01de30791db897bc7df732123305b
noell <noell@DESKTOP-NLQ10I7>
2023-10-26 18:11:14 +0200

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Uppgift_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur lång är låten? Svara med två heltal");
+            int minut=int.Parse(Console.ReadLine());
+
+            int sekund=int.Parse(Console.ReadLine());
+
+            int x = minut * 60;
+            int y = x + sekund;
+
+            if (y <= 260 && y >= 165)
+            {
+                Console.WriteLine("Låten får spelas på radiostationen.");
+            }
+            else
+            {
+                Console.WriteLine("Låten får tyvärr inte spelas på radiostationen.");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
